d0164f15c4b504169a2eb15282dd355516ee583e
Kalin Krumov <kaweeo@gmail.com>
2024-11-20 18:56:52 +0200

    chor: Update html templates

--- petstagram/accounts/models/app_profile.py
+++ petstagram/accounts/models/app_profile.py
@@ -34,3 +34,8 @@ class Profile(models.Model):
         null=True,
     )
 
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+
+        return "Anonymous"
